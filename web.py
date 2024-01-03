ecea70080da768e45ff8d95963c7766b7a641469
sntathe <sntathe@gmail.com>
2024-01-03 14:36:22 +0530

    Created a interactive web interface

--- web.py
+++ web.py
@@ -3,12 +3,26 @@ import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My Todo App")
-st.subheader("Thi is  todo app")
+st.subheader("This is  todo app")
 st.write("This app is useful for productivity")
 
-for item in todos:
-    st.checkbox(item)
+for index, item in enumerate(todos):
+    checkbox = st.checkbox(item, key=item)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[item]
+        st.experimental_rerun()
 
-st.text_input(label="" , placeholder="Enter a new to do")
+st.text_input(label="", placeholder="Enter a new to do",
+              on_change=add_todo, key='new_todo')
 
+st.session_state
